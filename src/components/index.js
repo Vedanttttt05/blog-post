61a58d50dc9049ba769a1176b89205e7699f56f7
Vedanttttt05 <vedantpatil05052004@gmail.com>
2025-10-11 19:45:08 +0530

    Add reusable Input, Header with dynamic nav, Logout button, and styled Footer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Header from "./header/header"
 import Footer from "./footer/footer"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./header/LogoutBtn"
 
 
-export {Header , Footer}
+export {Header , Footer , Container , Logo}
