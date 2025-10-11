790483bea3757349d3e4574103ba680d4de06679
Vedanttttt05 <vedantpatil05052004@gmail.com>
2025-10-11 12:27:42 +0530

    Setup Redux for auth state and integrate Appwrite authentication flow in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-
+import {useDispatch} from "react-redux"
+import { useEffect, useState } from 'react'
 import './App.css'
+import authService from "./appwrite/auth"
+import { login , logout } from "./store/authSlice"
+import { Header , Footer } from "./components"
 
 function App() {
+  const [loading , setLoading]   = useState(true)
+  const dispatch = useDispatch()
 
-  return (
+  useEffect(() =>{
+    authService.getCurrentUser().then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    }).finally(() => setLoading(false))
 
-    
-    <>
-    <h1>{}</h1>
-    </>
-  )
+  } , [])
+  
+return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header/>
+      <main>
+        <h1>giga</h1>
+      </main>
+      <Footer/>
+  </div>
+  </div> 
+) : null
 }
 
 export default App
